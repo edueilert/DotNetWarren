f49c20526b12241f33a43dc0a96a4c4b39b54bc8
Eduardo Rodrigues <eduardofutrodrigues@hotmail.com>
2022-06-20 17:10:57 -0300

    terceiro commit

--- src/Customer.cs
+++ src/Customer.cs
@@ -1,10 +1,40 @@
-public final class Singleton {
+public sealed class SingletonGenerico<G> where G : class, new()
+{
+    private static G _instance;
 
-    private static final Singleton INSTANCE = new Singleton();
-  
-    private Singleton() {}
-  
-    public static Singleton getInstance() { 
-      return INSTANCE;
+    public static G GetInstance()
+    {
+        lock (typeof(G))
+        {
+            if (_instance == null)
+                _instance = new G();
+
+            return _instance;
+        }
     }
-  }
+}
+
+// Teste do padrão Singleton
+public class Person { public string Name { get; set; } }
+public class Person { public string Email { get; set; } }
+public class Person { public string EmailC { get; set; } }
+public class Person { public string CPF { get; set; } }
+public class Person { public string Celphone { get; set; } }
+public class Person { public DateTime Nascimento { get; set; } }
+public class Person { public bool Whatsapp { get; set; } }
+public class Person { public bool EmailSms { get; set; } }
+public class Person { public string Pais { get; set; } }
+public class Person { public string Cidade { get; set; } }
+public class Person { public string CEP { get; set; } }
+public class Person { public string Endereco { get; set; } }
+public class Person { public int Numero { get; set; } }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person per = SingletonGenerico<Person>.GetInstance();
+        per.Nome = "João";
+
+    }
+}
