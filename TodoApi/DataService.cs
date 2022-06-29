fd1fc79c4992cca1ee8c049fe22b0429512d726f
Eduardo Rodrigues <eduardofutrodrigues@hotmail.com>
2022-06-29 17:15:57 -0300

    setimo commit 2

--- TodoApi/DataService.cs
+++ TodoApi/DataService.cs
@@ -7,7 +7,12 @@ namespace TudoApi
         public List<WeatherForecast> Data { get; set; } = new List<WeatherForecast>();
         public void Add()
         {
-            // Data.Add
+        Data.Add(new WeatherForecast() { Id = 1234, nomeCompleto = "Eduardo Rodrigues", email = "edurodrigues51@gmail.com", emailConfirmation = "edurodrigues51@gmail.com", cpf = "87405873049", cellphone = "51985784290"});
+        Data.Add(new WeatherForecast() { PartName = "chain ring", PartId = 1334 });
+        Data.Add(new WeatherForecast() { PartName = "regular seat", PartId = 1434 });
+        Data.Add(new WeatherForecast() { PartName = "banana seat", PartId = 1444 });
+        Data.Add(new WeatherForecast() { PartName = "cassette", PartId = 1534 });
+        Data.Add(new WeatherForecast() { PartName = "shift lever", PartId = 1634 });
         }
     }
 }
--- TodoApi/WeatherForecast.cs
+++ TodoApi/WeatherForecast.cs
@@ -1,5 +1,4 @@
 using System;
-
 namespace TodoApi;
 
 public class WeatherForecast
